64dc17c8b7bf27d855fd89cedd2e02bb400abe85
Aleksi Backman <bddubbb@gmail.com>
2019-04-25 20:54:07 +0300

    Replace cli script placeholder

---
+++ scripts/enchatter.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+print("Eventually used for providing a command line interface to Enchatter.\n");
+print("Create configuration file based on command line arguments and give this to Enchatter.\n");
+print("Might do runtime configuration by connecting to already running Enchatter via socket.\n");
--- scripts/enchatter.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python2
-
-print "Eventually used for providing a command line interface to Enchatter."
-print "Create configuration file based on command line arguments and give this to Enchatter."
-print "Might do runtime configuration by connecting to already running Enchatter via socket."
